fb47791e4208263269af323f8fb58baab0e03514
Frank S. Schierle <git@frankschierle.de>
2018-04-27 18:13:50 +0200

    ThemeManger class added.
    The ThemeManager class provides a method to get all themes installed in Visual Studio.

---
+++ ThemeSwitcher/Logic/Theme.cs
@@ -0,0 +1,32 @@
+﻿namespace ThemeSwitcher.Logic
+{
+  using System.Diagnostics;
+
+  /// <summary>Represents a Visual Studio theme.</summary>
+  [DebuggerDisplay("{DisplayName}")]
+  internal class Theme
+  {
+    #region Constructors and Destructors
+
+    /// <summary>Initializes a new instance of the <see cref="Theme" /> class.</summary>
+    /// <param name="id">The id of the theme.</param>
+    /// <param name="displayName">The display name of the theme.</param>
+    public Theme(string id, string displayName)
+    {
+      this.Id = id;
+      this.DisplayName = displayName;
+    }
+
+    #endregion
+
+    #region Public Properties
+
+    /// <summary>Gets the id of the theme.</summary>
+    public string Id { get; }
+
+    /// <summary>Gets the display name of the theme.</summary>
+    public string DisplayName { get; }
+
+    #endregion
+  }
+}
---
+++ ThemeSwitcher/Logic/ThemeManager.cs
@@ -0,0 +1,57 @@
+﻿namespace ThemeSwitcher.Logic
+{
+  using System.Collections.Generic;
+
+  using EnvDTE;
+
+  using Microsoft.VisualStudio.Shell;
+  using Microsoft.Win32;
+
+  /// <summary>Provides methods to manage installed Visual Studio themes.</summary>
+  internal class ThemeManager
+  {
+    #region Properties
+
+    /// <summary>Gets the DTE automation object.</summary>
+    private DTE Dte
+    {
+      get { return (DTE)Package.GetGlobalService(typeof(DTE)); }
+    }
+
+    #endregion
+
+    #region Public Methods and Operators
+
+    /// <summary>Gets all themes installed in Visual Studio.</summary>
+    /// <returns>All themes installed in Visual Studio.</returns>
+    public IEnumerable<Theme> GetInstalledThemes()
+    {
+      string registryPath = this.Dte.RegistryRoot + "_Config\\Themes";
+      var themes = new List<Theme>();
+      string[] installedThemesKeys;
+
+      using (RegistryKey themesKey = Registry.CurrentUser.OpenSubKey(registryPath))
+      {
+        if (themesKey != null)
+        {
+          installedThemesKeys = themesKey.GetSubKeyNames();
+
+          foreach (string key in installedThemesKeys)
+          {
+            using (RegistryKey themeKey = themesKey.OpenSubKey(key))
+            {
+              if (themeKey != null)
+              {
+                themes.Add(new Theme(key, themeKey.GetValue(null).ToString()));
+              }
+            }
+          }
+        }
+      }
+
+      return themes;
+    }
+
+    #endregion
+  }
+}
