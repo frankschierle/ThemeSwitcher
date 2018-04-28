ff130207c334ce78a18cadad7a45b48b5a86d1a3
Frank S. Schierle <git@frankschierle.de>
2018-04-28 10:36:53 +0200

    Command to switch theme and window layout added
    Just the command stub, no logic.

---
+++ ThemeSwitcher/SwitchThemeAndWindowLayoutCommand.cs
@@ -0,0 +1,110 @@
+﻿namespace ThemeSwitcher
+{
+  using System;
+  using System.ComponentModel.Design;
+  using System.Globalization;
+
+  using Microsoft.VisualStudio.Shell;
+  using Microsoft.VisualStudio.Shell.Interop;
+
+  /// <summary>Command handler to switch theme and window layout.</summary>
+  internal sealed class SwitchThemeAndWindowLayoutCommand
+  {
+    #region Constants
+
+    /// <summary>The command id.</summary>
+    private const int CommandId = 0x0100;
+
+    #endregion
+
+    #region Static Fields
+
+    /// <summary>The command menu group (command set GUID).</summary>
+    private static readonly Guid CommandSet = new Guid("c98bea8a-94eb-4dea-8d12-234f3d938d6d");
+
+    #endregion
+
+    #region Fields
+
+    /// <summary>The VS package that provides this command.</summary>
+    private readonly Package package;
+
+    #endregion
+
+    #region Constructors and Destructors
+
+    /// <summary> Initializes a new instance of the <see cref="SwitchThemeAndWindowLayoutCommand" /> class.</summary>
+    /// <param name="package">Owner package.</param>
+    /// <exception cref="ArgumentNullException">Occurs if <paramref name="package" /> is null.</exception>
+    private SwitchThemeAndWindowLayoutCommand(Package package)
+    {
+      OleMenuCommandService commandService;
+
+      if (package == null)
+      {
+        throw new ArgumentNullException(nameof(package));
+      }
+
+      this.package = package;
+
+      commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+
+      if (commandService != null)
+      {
+        commandService.AddCommand(new MenuCommand(this.MenuItemCallback, new CommandID(CommandSet, CommandId)));
+      }
+    }
+
+    #endregion
+
+    #region Public Properties
+
+    /// <summary>Gets the instance of the command.</summary>
+    public static SwitchThemeAndWindowLayoutCommand Instance { get; private set; }
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>Gets the service provider from the owner package.</summary>
+    private IServiceProvider ServiceProvider
+    {
+      get { return this.package; }
+    }
+
+    #endregion
+
+    #region Public Methods and Operators
+
+    /// <summary>Initializes the singleton instance of the command.</summary>
+    /// <param name="package">Owner package, not null.</param>
+    public static void Initialize(Package package)
+    {
+      Instance = new SwitchThemeAndWindowLayoutCommand(package);
+    }
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>This function is the callback used to execute the command when the menu item is clicked.</summary>
+    /// <param name="sender">The event sender.</param>
+    /// <param name="e">The event args.</param>
+    private void MenuItemCallback(object sender, EventArgs e)
+    {
+      string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+      string title = "SwitchThemeAndWindowLayoutCommand";
+
+      // Show a message box to prove we were here
+      VsShellUtilities.ShowMessageBox(
+        this.ServiceProvider,
+        message,
+        title,
+        OLEMSGICON.OLEMSGICON_INFO,
+        OLEMSGBUTTON.OLEMSGBUTTON_OK,
+        OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+    }
+
+    #endregion
+  }
+}
--- ThemeSwitcher/ThemeSwitcherPackage.cs
+++ ThemeSwitcher/ThemeSwitcherPackage.cs
@@ -11,12 +11,27 @@
   [PackageRegistration(UseManagedResourcesOnly = true)]
   [ProvideOptionPage(typeof(ThemeSwitcherOptionsDialogPage), "Theme Switcher", "General", 0, 0, true)]
   [InstalledProductRegistration("Theme Switcher", "A Visual Studio extension that allows users to fast switch between themes and window layouts.", "1.0")]
+  [ProvideMenuResource("Menus.ctmenu", 1)]
   public sealed class ThemeSwitcherPackage : Package
   {
     #region Constants
 
     /// <summary>Gets the ThemeSwitcherPackage GUID string.</summary>
     public const string PackageGuidString = "49a61599-600e-49ec-a363-7009c2a66a4f";
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>Initialization of the package; this method is called right
+    /// after the package is sited, so this is the place where you can put all
+    /// the initialization code that rely on services provided by VisualStudio.
+    /// </summary>
+    protected override void Initialize()
+    {
+      base.Initialize();
+      SwitchThemeAndWindowLayoutCommand.Initialize(this);
+    }
 
     #endregion
   }
